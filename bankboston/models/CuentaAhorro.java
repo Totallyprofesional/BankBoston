0a5c406eb498eca4dc69f7e340c9a8df25d85f24
Totallypro <pablcar@gmail.com>
2025-06-01 21:54:11 -0400

    Add files via upload

---
+++ bankboston/models/CuentaAhorro.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankboston.models; 
+import java.time.LocalDate;
+
+public class CuentaAhorro extends Cuenta {
+    private LocalDate fechaCreación;
+    
+    public CuentaAhorro (int saldo, int monto){ 
+        super(saldo, monto);
+        this.fechaCreación = LocalDate.now();
+    }   
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("Cuenta Ahorro: " + cuentaCorriente);
+        System.out.println("Saldo: " + saldo);
+    } 
+  
+    // Bloquea giro hasta 6 meses desde creacion de la cuenta
+    @Override
+    public void especialGiro(int valor) {
+        relojRetiro(valor);   
+    }
+    
+    public void relojRetiro (int valor){    
+        LocalDate día = LocalDate.now();
+        LocalDate fechaRetiro = fechaCreación.plusMonths(6);
+        
+        if (día == fechaRetiro){
+            super.girarSaldo(monto);       
+        } else { 
+            System.out.println("No se puede girar saldo. Fecha de retiro: " + fechaRetiro);
+        }          
+    }
+    
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+}
+
---
+++ bankboston/models/CuentaPremium.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bankboston.models;
+
+public class CuentaPremium extends Cuenta {
+    private int creditoPremium = 500000;
+    private double valor;
+ 
+    public CuentaPremium (int saldo, int monto, int creditoPremium, double valor){ 
+        super(saldo, monto);
+        this.creditoPremium = creditoPremium;
+        this.saldo += creditoPremium;
+        this.valor =  saldo * 0.03;
+    }   
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("Cuenta Premium: " + cuentaCorriente);
+        System.out.println("Saldo: " + saldo);
+    }
+    
+    // Cobra porcentaje de saldo por 
+    @Override
+    public void especialGiro(int valor) {
+        this.saldo += valor;
+    }
+  
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+}
+
